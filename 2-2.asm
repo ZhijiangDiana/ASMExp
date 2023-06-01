c2d3a630e380f02f6999edea9231838845728c3e
嘉然今天吃向晚 <45503719+ZhijiangDiana@users.noreply.github.com>
2023-06-01 22:45:58 +0800

    左右移位实验

---
+++ 2-2.asm
@@ -0,0 +1,48 @@
+DATAS SEGMENT
+	X DW ?
+	Y DW 1
+	Z DW 256
+	CON DW 64
+	CNT DW 0
+	
+    ;此处输入数据段代码  
+DATAS ENDS
+
+STACKS SEGMENT
+    ;此处输入堆栈段代码
+STACKS ENDS
+
+CODES SEGMENT
+    ASSUME CS:CODES,DS:DATAS,SS:STACKS
+START:
+    MOV AX,DATAS
+    MOV DS,AX
+    MOV BL,2
+    ;此处输入代码段代码
+    
+;计算log2(CON)并存入CNT中
+LOG2:
+	CMP CON,1
+	JE EXIT
+	MOV AX,CON
+	DIV BL
+	MOV AH,0
+	MOV CON,AX
+	INC CNT
+	JMP LOG2	
+EXIT:
+
+
+	MOV CX,CNT
+	SHL Y,CL
+	SHR Z,CL
+	MOV AX,Y
+	ADD AX,Z
+	MOV X,AX
+	
+    
+    
+    MOV AH,4CH
+    INT 21H
+CODES ENDS
+    END START
